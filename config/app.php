d625d023055e6d448aa2f7f1a229768d0bd0fb63
Jeremiah Ashley <jeremiah.ashley@live.com>
2023-04-02 22:54:26 -0500

    WIP: Time Logs

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Package Service Providers...
